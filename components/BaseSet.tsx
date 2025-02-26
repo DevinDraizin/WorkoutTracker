5802deb63c0b3329d1364af638efb979b4f4de95
devindraizin <devindraizin@knights.ucf.edu>
2025-02-25 22:17:19 -0500

    Added animations and built out standard set but disable logic is broken

--- components/BaseSet.tsx
+++ components/BaseSet.tsx
@@ -1,3 +1,48 @@
-export interface BaseSetProps {
-  
+import React, { useEffect, useRef, useState } from 'react'
+import { View, StyleSheet, ViewStyle, Animated } from 'react-native'
+import { LayoutAnimation } from 'react-native'
+
+interface BaseSetProps {
+  children: React.ReactNode
+  style?: ViewStyle
+  animateResize?: boolean
 }
+
+const BaseSet: React.FC<BaseSetProps> = (componentProps: BaseSetProps) => {
+
+    const animationDuration = 300
+ 
+    useEffect(() => {
+        const animConfig = {
+            duration: animationDuration,
+            update: {
+            type: LayoutAnimation.Types.easeInEaseOut,
+            property: LayoutAnimation.Properties.scaleXY,
+            },
+        }
+        LayoutAnimation.configureNext(animConfig)
+    }, [componentProps.animateResize])
+
+  return <View style={[styles.card, componentProps.style]}>{componentProps.children}</View>
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: 16,
+    margin: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+})
+
+export default BaseSet
---
+++ components/StandardSet.tsx
@@ -0,0 +1,88 @@
+import { Switch, View, Text, StyleSheet } from "react-native"
+import BaseSet from "./BaseSet"
+import { useEffect, useState } from "react"
+import WTTextInput from "./WTCore/WTTextInput"
+import { StandardSetDetails } from "@/Types/DBTypes"
+import WTButton, { ButtonVariant } from "./WTCore/WTButton"
+
+interface StandardSetProps {
+  currentSet: StandardSetDetails
+  previousSet?: StandardSetDetails | undefined
+}
+
+const StandardSet = (componentProps: StandardSetProps) => {
+    const [dropSetEnabled, setDropSetEnabled] = useState(false)
+    const [reps, setReps] = useState<string>(componentProps.previousSet ? componentProps.previousSet.reps.toString() : '')
+    const [weight, setWeight] = useState<string>(componentProps.previousSet ? componentProps.previousSet.weight.toString() : '')
+    const [dropReps, setDropReps] = useState<string>(componentProps.previousSet &&  componentProps.previousSet.isDropset ? componentProps.previousSet.dropsetReps!.toString() : '')
+    const [dropWeight, setDropWeight] = useState<string>(componentProps.previousSet && componentProps.previousSet.isDropset ? componentProps.previousSet.dropsetWeight!.toString() : '')
+    const [isComplete, setIsComplete] = useState<boolean>(false)
+
+
+    //This shit is so broken because dropSetEnabled is the key for 
+    const updateIsComplete = () => {
+        if (dropSetEnabled) {
+            setIsComplete(reps.trim() !== "" && weight.trim() !== "" && dropReps.trim() !== "" && dropWeight.trim() !== "")
+        } else {
+            setIsComplete(reps.trim() !== "" && weight.trim() !== "")
+        }
+    }
+
+    useEffect(() => {
+        updateIsComplete()
+    }, [reps, weight, dropReps, dropWeight])
+
+
+    const onDropSettToggle = () => {
+        
+        updateIsComplete()
+        setDropSetEnabled(!dropSetEnabled)
+    }
+
+    const buildDropSet = () => {
+        return (
+            <View style={styles.fullWidth}>
+                <WTTextInput numberInput={true} cantBeEmpty={true} prompt='Drop Set Weight' value={dropWeight} onChange={(newValue: string) => {setDropWeight(newValue)}} />
+                <WTTextInput numberInput={true} cantBeEmpty={true} prompt='Drop Set Reps' value={dropReps} onChange={(newValue: string) => {setDropReps(newValue)}} />
+            </View>
+        )
+    }
+
+    return (
+        <BaseSet animateResize={dropSetEnabled}>
+            <WTTextInput numberInput={true} cantBeEmpty={true} prompt='Set Weight' value={weight} onChange={(newValue: string) => {setWeight(newValue)}} />
+            <WTTextInput numberInput={true} cantBeEmpty={true} prompt='Set Reps' value={reps} onChange={(newValue: string) => {setReps(newValue)}} />
+            <View style={styles.row}>
+                <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor={'#007AFF'}
+                    ios_backgroundColor="#acadad"
+                    onValueChange={onDropSettToggle}
+                    value={dropSetEnabled}
+                />
+                <Text style={styles.text}>Drop Set</Text>
+            </View>
+
+            {dropSetEnabled && buildDropSet()}
+            <View style={styles.fullWidth}>
+                <WTButton disabled={!isComplete} title="Finish" onPress={() => { } } variant={ButtonVariant.Primary} />
+            </View>
+        </BaseSet>
+    )
+}
+
+const styles = StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+      alignItems: 'center', // Align items vertically in the row
+      marginBottom: 10,
+    },
+    text: {
+      marginLeft: 10, // Add some spacing between the switch and text
+    },
+    fullWidth: {
+        width: '100%',
+    }
+  });
+
+export default StandardSet
